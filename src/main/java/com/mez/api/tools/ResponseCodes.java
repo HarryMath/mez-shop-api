4542e8e3a5764c37c28a31946af3b78509de4066
RedmiBook <nikitabort22092000@gmail.com>
2021-04-01 21:06:53 +0300

    telegram form

---
+++ src/main/java/com/mez/api/tools/ResponseCodes.java
@@ -0,0 +1,11 @@
+package com.mez.api.tools;
+
+public class ResponseCodes {
+    public static final byte SUCCESS = 1;
+    public static final byte DATABASE_ERROR = -1;
+    public static final byte INCORRECT_USERNAME_OR_PASSWORD = -2;
+    public static final byte UNAUTHORISED = -3;
+    public static final byte NO_ACCESS = -4;
+    public static final byte CLOUDINARY_ERROR = -7;
+    public static final byte UNKNOWN_ERROR = -8;
+}
